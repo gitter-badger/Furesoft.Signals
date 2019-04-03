161d81327f2ef5ee029b8e579c0e3ca18f003af0
furesoft <filmee24@gmail.com>
2019-04-03 13:47:08 +0200

    added attributes

--- Furesoft.Signals/Signal.cs
+++ Furesoft.Signals/Signal.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using Furesoft.Signals.Core;
 using Newtonsoft.Json;
 
