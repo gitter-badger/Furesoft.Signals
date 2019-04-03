de1a837c924b0ce4be44e1fdd675bc3409f4131d
furesoft <filmee24@gmail.com>
2019-04-03 13:56:21 +0200

    added shared attribute to test

--- TestSender/Program.cs
+++ TestSender/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Furesoft.Signals;
+using Furesoft.Signals.Attributes;
 using System;
 using TestModels;
 
@@ -11,10 +12,12 @@ namespace TestSender
             var channel = Signal.CreateRecieverChannel("signals.test");
 
             Signal.CallEvent(channel, new PingArg { Message = "hello world" });
+            Signal.CollectShared(channel);
 
             Console.ReadLine();
         }
 
+        [SharedFunction(0xC0FFEE)]
         private static PingArg Pong(PingArg arg)
         {
             return new PingArg { Message = arg.Message + "/PONG" };
